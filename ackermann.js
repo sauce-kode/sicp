fde78d095304e17293c18eb5821e0acc716d50c7
sauce-kode <dblacktin@gmail.com>
2021-01-13 22:05:42 +0100

    Fixes indentation

--- ackermann.js
+++ ackermann.js
@@ -2,10 +2,10 @@ function A(x, y) {
     return y === 0 
         ? 0
         : x === 0
-        ? 2 * y
-        : y === 1
-        ? 2
-        : A(x - 1, A(x, y - 1))
+         ? 2 * y
+         : y === 1
+          ? 2
+          : A(x - 1, A(x, y - 1))
 }
 
 console.log(A(2, 4))
